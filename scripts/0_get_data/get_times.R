83fd050aade5872f9bc0c2a0cb46c70bf176667f
calebchou21 <calebchou21@gmail.com>
2023-05-26 20:45:43 -0600

    Added women's times

--- scripts/0_get_data/get_times.R
+++ scripts/0_get_data/get_times.R
@@ -13,10 +13,16 @@ load("data/events.Rdata")
 driver <- rsDriver(browser = "firefox", chromever = NULL, iedrver = NULL, verbose = TRUE)
 remote_driver <- driver$client
 
-## empty storage
-df_overall <- data.frame()
-df_qual <- data.frame()
-df_final <- data.frame()
+## empty storage for mens times
+df_overall_m <- data.frame()
+df_qual_m <- data.frame()
+df_final_m <- data.frame()
+
+## empty storage for womens times
+df_overall_w <- data.frame()
+df_qual_w <- data.frame()
+df_final_w <- data.frame()
+
 for(i in seq_len(nrow(events))) {
   e_id <- events[i, "event_id"]
   if(!is.na(e_id)) {
@@ -25,12 +31,12 @@ for(i in seq_len(nrow(events))) {
     ## mens times ----
     remote_driver$navigate(paste0("https://components.ifsc-climbing.org/result-complete/?event=", e_id, "&result=2"))
     Sys.sleep(2)
-    
+
     ## overall table
     table_elm <- remote_driver$findElements(using = "css", value = "#table_id")
     table_overall <- read_html(table_elm[[1]]$getElementAttribute("outerHTML")[[1]]) # get html
     df_overall_men <- rvest::html_table(table_overall)[[1]]
-    
+
     ## cleanup table
     if(ncol(df_overall_men) > 0) {
       if(ncol(df_overall_men) == 6) {
@@ -46,7 +52,7 @@ for(i in seq_len(nrow(events))) {
           mutate(best_qual = ifelse(class(best_qual) == "character", parse_number(best_qual), best_qual)) |>
           mutate(final = as.numeric(final)) |>
           mutate(sex = "M", event_id = e_id) |>
-          bind_rows(df_overall) -> df_overall
+          bind_rows(df_overall_m) -> df_overall_m
       } else {
         df_overall_men |>
           rename(rank = Rank, fname = Name, lname = 3, country = Country, best_qual = Qualification) |>
@@ -57,10 +63,10 @@ for(i in seq_len(nrow(events))) {
           mutate(best_qual = ifelse(class(best_qual) == "character", parse_number(best_qual), best_qual)) |>
           mutate(final = NA) |>
           mutate(sex = "M", event_id = e_id) |>
-          bind_rows(df_overall) -> df_overall
+          bind_rows(df_overall_m) -> df_overall_m
       }
     }
-    
+
     ## qualifiers
     qual_btn <- remote_driver$findElements(using = 'xpath', "//a[.='Qualification']")
     if(length(qual_btn) > 0) {
@@ -72,7 +78,7 @@ for(i in seq_len(nrow(events))) {
     } else {
       df_qual_men <- data.frame()
     }
-    
+
     ## cleanup
     if(ncol(df_qual_men) > 0) {
       if(ncol(df_qual_men) == 6) {
@@ -88,16 +94,16 @@ for(i in seq_len(nrow(events))) {
                  lane_b = ifelse(tolower(lane_b) == "fall", NA, lane_b)) |>
           mutate(across(.cols = c(lane_a, lane_b), .fns = as.numeric)) |>
           mutate(sex = "M", event_id = e_id) |>
-          bind_rows(df_qual) -> df_qual
+          bind_rows(df_qual_m) -> df_qual_m
       } else {
         df_qual_men |>
           rename(rank = Rank, fname = Name, lname = 3, country = Country) |>
           mutate(fname = tolower(fname), lname = tolower(lname), lane_a = NA, lane_b = NA) |>
           mutate(sex = "M", event_id = e_id) |>
-          bind_rows(df_qual) -> df_qual
+          bind_rows(df_qual_m) -> df_qual_m
       }
     }
-    
+
     ## finals
     final_btn <- remote_driver$findElements(using = 'xpath', "//a[.='Final']")
     if(length(final_btn) > 0) {
@@ -109,7 +115,7 @@ for(i in seq_len(nrow(events))) {
     } else {
       df_final_men <- data.frame()
     }
-    
+
     ## cleanup table
     if(ncol(df_final_men) > 0) {
       if("1/8" %in% names(df_final_men)) {
@@ -121,7 +127,7 @@ for(i in seq_len(nrow(events))) {
           mutate(across(first_round:big_final, function(col) { ifelse(col == "FALL", NA, col)})) |>
           mutate(across(first_round:big_final, as.numeric)) |>
           mutate(sex = "M", event_id = e_id) |>
-          bind_rows(df_final) -> df_final
+          bind_rows(df_final_m) -> df_final_m
       } else {
         df_final_men |>
           rename(rank = Rank, fname = Name, lname = 3, country = Country, quarter = `1/4`, semi = `1/2`, small_final = `Small Final`, big_final = Final) |>
@@ -131,118 +137,141 @@ for(i in seq_len(nrow(events))) {
           mutate(across(quarter:big_final, function(col) { ifelse(col == "FALL", NA, col)})) |>
           mutate(across(quarter:big_final, as.numeric)) |>
           mutate(sex = "M", first_round = NA, fall_first_round = NA, false_start_first_round = NA, event_id = e_id) |>
-          bind_rows(df_final) -> df_final
+          bind_rows(df_final_m) -> df_final_m
+      }
+    }
+
+    
+    # womens times ----
+    remote_driver$navigate(paste0("https://components.ifsc-climbing.org/result-complete/?event=", e_id, "&result=6"))
+    Sys.sleep(2)
+    
+    ## overall table
+    table_elm <- remote_driver$findElements(using = "css", value = "#table_id")
+    table_overall <- read_html(table_elm[[1]]$getElementAttribute("outerHTML")[[1]]) # get html
+    df_overall_women <- rvest::html_table(table_overall)[[1]]
+    
+    ## cleanup table
+    if(ncol(df_overall_women) > 0) {
+      if(ncol(df_overall_women) == 6) {
+        df_overall_women |>
+          rename(rank = Rank, fname = Name, lname = 3, country = Country, best_qual = Qualification, final = Final) |>
+          mutate(fname = tolower(fname), lname = tolower(lname)) |>
+          mutate(fall_qual = tolower(best_qual) == "fall") |>
+          mutate(false_start_qual = tolower(best_qual) == "false start") |>
+          mutate(fall_final = tolower(final) == "fall") |>
+          mutate(false_start_final = tolower(final) == "false start") |>
+          mutate(best_qual = ifelse(tolower(best_qual) == "fall", NA, best_qual),
+                 final = ifelse(tolower(final) == "fall", NA, final)) |>
+          mutate(best_qual = ifelse(class(best_qual) == "character", parse_number(best_qual), best_qual)) |>
+          mutate(final = as.numeric(final)) |>
+          mutate(sex = "F", event_id = e_id) |>
+          bind_rows(df_overall_w) -> df_overall_w
+      } else {
+        df_overall_women |>
+          rename(rank = Rank, fname = Name, lname = 3, country = Country, best_qual = Qualification) |>
+          mutate(fname = tolower(fname), lname = tolower(lname)) |>
+          mutate(fall_qual = tolower(best_qual) == "fall") |>
+          mutate(false_start_qual = tolower(best_qual) == "false start") |>
+          mutate(best_qual = ifelse(tolower(best_qual) == "fall", NA, best_qual)) |>
+          mutate(best_qual = ifelse(class(best_qual) == "character", parse_number(best_qual), best_qual)) |>
+          mutate(final = NA) |>
+          mutate(sex = "F", event_id = e_id) |>
+          bind_rows(df_overall_w) -> df_overall_w
       }
     }
     
-    ## womens times ----
-    # remote_driver$navigate(paste0("https://components.ifsc-climbing.org/result-complete/?event=", e_id, "&result=6"))
-    # Sys.sleep(2)
-    # 
-    # ## overall table
-    # table_elm <- remote_driver$findElements(using = "css", value = "#table_id")
-    # table_overall <- read_html(table_elm[[1]]$getElementAttribute("outerHTML")[[1]]) # get html
-    # df_overall_women <- rvest::html_table(table_overall)[[1]]
-    # 
-    # ## cleanup table
-    # if(ncol(df_overall_women) > 0) {
-    #   if(ncol(df_overall_women) == 6) {
-    #     df_overall_women |>
-    #       rename(rank = Rank, fname = Name, lname = 3, country = Country, best_qual = Qualification, final = Final) |>
-    #       mutate(fname = tolower(fname), lname = tolower(lname)) |>
-    #       mutate(best_qual = ifelse(tolower(best_qual) == "fall", NaN, best_qual),
-    #              final = ifelse(tolower(final) == "fall", NaN, final)) |>
-    #       mutate(best_qual = ifelse(class(best_qual) == "character", parse_number(best_qual), best_qual)) |>
-    #       mutate(final = as.numeric(final)) |>
-    #       mutate(sex = "F", event_id = e_id) |>
-    #       bind_rows(df_overall) -> df_overall
-    #   } else {
-    #     df_overall_women |>
-    #       rename(rank = Rank, fname = Name, lname = 3, country = Country, best_qual = Qualification) |>
-    #       mutate(fname = tolower(fname), lname = tolower(lname)) |>
-    #       mutate(best_qual = ifelse(tolower(best_qual) == "fall", NaN, best_qual)) |>
-    #       mutate(best_qual = ifelse(class(best_qual) == "character", parse_number(best_qual), best_qual)) |>
-    #       mutate(final = NA) |>
-    #       mutate(sex = "F", event_id = e_id) |>
-    #       bind_rows(df_overall) -> df_overall
-    #   }
-    # }
-    # 
-    # ## qualifiers
-    # qual_btn <- remote_driver$findElements(using = 'xpath', "//a[.='Qualification']")
-    # if(length(qual_btn) > 0) {
-    #   qual_btn[[1]]$clickElement()
-    #   Sys.sleep(2)
-    #   table_elm <- remote_driver$findElements(using = "css", value = "#table_id")
-    #   table_qual <- read_html(table_elm[[1]]$getElementAttribute("outerHTML")[[1]]) # get html
-    #   df_qual_women <- rvest::html_table(table_qual)[[1]]
-    # } else {
-    #   df_qual_women <- data.frame()
-    # }
-    # 
-    # ## cleanup
-    # if(ncol(df_qual_women) > 0) {
-    #   if(ncol(df_qual_women) == 6) {
-    #     ## sometimes there are not actually times in the qualifiers (old results)
-    #     df_qual_women |>
-    #       rename(rank = Rank, fname = Name, lname = 3, country = Country, lane_a = `Lane A`, lane_b = `Lane B`) |>
-    #       mutate(fname = tolower(fname), lname = tolower(lname)) |>
-    #       mutate(lane_a = ifelse(tolower(lane_a) == "fall", NaN, lane_a),
-    #              lane_b = ifelse(tolower(lane_b) == "fall", NaN, lane_b)) |>
-    #       mutate(across(.cols = c(lane_a, lane_b), .fns = as.numeric)) |>
-    #       mutate(sex = "M", event_id = e_id) |>
-    #       bind_rows(df_qual) -> df_qual
-    #   } else {
-    #     df_qual_women |>
-    #       rename(rank = Rank, fname = Name, lname = 3, country = Country) |>
-    #       mutate(fname = tolower(fname), lname = tolower(lname), lane_a = NA, lane_b = NA) |>
-    #       mutate(sex = "M", event_id = e_id) |>
-    #       bind_rows(df_qual) -> df_qual
-    #   }
-    # }
-    # 
-    # ## finals
-    # final_btn <- remote_driver$findElements(using = 'xpath', "//a[.='Final']")
-    # if(length(final_btn) > 0) {
-    #   final_btn[[1]]$clickElement()
-    #   Sys.sleep(2)
-    #   table_elm <- remote_driver$findElements(using = "css", value = "#table_id")
-    #   table_final <- read_html(table_elm[[1]]$getElementAttribute("outerHTML")[[1]]) # get html
-    #   df_final_women <- rvest::html_table(table_final)[[1]]
-    # } else {
-    #   df_final_women <- data.frame()
-    # }
-    # 
-    # ## cleanup table
-    # if(ncol(df_final_women) > 0) {
-    #   if("1/8" %in% names(df_final_women)) {
-    #     df_final_women |>
-    #       rename(rank = Rank, fname = Name, lname = 3, country = Country, first_round = `1/8`, quarter = `1/4`, semi = `1/2`, small_final = `Small Final`, big_final = Final) |>
-    #       mutate(fname = tolower(fname), lname = tolower(lname)) |>
-    #       mutate(across(first_round:big_final, function(col) { ifelse(col == "FALL", NaN, col)})) |>
-    #       mutate(across(first_round:big_final, as.numeric)) |>
-    #       mutate(sex = "M", event_id = e_id) |>
-    #       bind_rows(df_final) -> df_final
-    #   } else {
-    #     df_final_women |>
-    #       rename(rank = Rank, fname = Name, lname = 3, country = Country, quarter = `1/4`, semi = `1/2`, small_final = `Small Final`, big_final = Final) |>
-    #       mutate(fname = tolower(fname), lname = tolower(lname)) |>
-    #       mutate(across(first_round:big_final, function(col) { ifelse(col == "FALL", NaN, col)})) |>
-    #       mutate(across(first_round:big_final, as.numeric)) |>
-    #       mutate(sex = "M", first_round = NA, event_id = e_id) |>
-    #       bind_rows(df_final) -> df_final
-    #   }
-    # }
+    ## qualifiers
+    qual_btn <- remote_driver$findElements(using = 'xpath', "//a[.='Qualification']")
+    if(length(qual_btn) > 0) {
+      qual_btn[[1]]$clickElement()
+      Sys.sleep(2)
+      table_elm <- remote_driver$findElements(using = "css", value = "#table_id")
+      table_qual <- read_html(table_elm[[1]]$getElementAttribute("outerHTML")[[1]]) # get html
+      df_qual_women <- rvest::html_table(table_qual)[[1]]
+    } else {
+      df_qual_women <- data.frame()
+    }
+    
+    ## cleanup
+    if(ncol(df_qual_women) > 0) {
+      if(ncol(df_qual_women) == 6) {
+        ## sometimes there are not actually times in the qualifiers (old results)
+        df_qual_women |>
+          rename(rank = Rank, fname = Name, lname = 3, country = Country, lane_a = `Lane A`, lane_b = `Lane B`) |>
+          mutate(fname = tolower(fname), lname = tolower(lname)) |>
+          mutate(fall_lane_a = tolower(lane_a) == "fall") |>
+          mutate(false_start_lane_a = tolower(lane_a) == "false start") |>
+          mutate(fall_lane_b = tolower(lane_b) == "fall") |>
+          mutate(false_start_lane_b = tolower(lane_b) == "false start") |>
+          mutate(lane_a = ifelse(tolower(lane_a) == "fall", NA, lane_a),
+                 lane_b = ifelse(tolower(lane_b) == "fall", NA, lane_b)) |>
+          mutate(across(.cols = c(lane_a, lane_b), .fns = as.numeric)) |>
+          mutate(sex = "F", event_id = e_id) |>
+          bind_rows(df_qual_w) -> df_qual_w
+      } else {
+        df_qual_women |>
+          rename(rank = Rank, fname = Name, lname = 3, country = Country) |>
+          mutate(fname = tolower(fname), lname = tolower(lname), lane_a = NA, lane_b = NA) |>
+          mutate(sex = "M", event_id = e_id) |>
+          bind_rows(df_qual_w) -> df_qual_w
+      }
+    }
+    
+    ## finals
+    final_btn <- remote_driver$findElements(using = 'xpath', "//a[.='Final']")
+    if(length(final_btn) > 0) {
+      final_btn[[1]]$clickElement()
+      Sys.sleep(2)
+      table_elm <- remote_driver$findElements(using = "css", value = "#table_id")
+      table_final <- read_html(table_elm[[1]]$getElementAttribute("outerHTML")[[1]]) # get html
+      df_final_women <- rvest::html_table(table_final)[[1]]
+    } else {
+      df_final_women <- data.frame()
+    }
+    
+    ## cleanup table
+    if(ncol(df_final_women) > 0) {
+      if("1/8" %in% names(df_final_women)) {
+        df_final_women |>
+          rename(rank = Rank, fname = Name, lname = 3, country = Country, first_round = `1/8`, quarter = `1/4`, semi = `1/2`, small_final = `Small Final`, big_final = Final) |>
+          mutate(fname = tolower(fname), lname = tolower(lname)) |>
+          mutate(across(first_round:big_final , function(col) { tolower(col) == "fall" }, .names = "fall_{.col}")) |>
+          mutate(across(first_round:big_final , function(col) { tolower(col) == "false start" }, .names = "false_start_{.col}")) |>
+          mutate(across(first_round:big_final, function(col) { ifelse(col == "FALL", NA, col)})) |>
+          mutate(across(first_round:big_final, as.numeric)) |>
+          mutate(sex = "F", event_id = e_id) |>
+          bind_rows(df_final_w) -> df_final_w
+      } else {
+        df_final_women |>
+          rename(rank = Rank, fname = Name, lname = 3, country = Country, quarter = `1/4`, semi = `1/2`, small_final = `Small Final`, big_final = Final) |>
+          mutate(fname = tolower(fname), lname = tolower(lname)) |>
+          mutate(across(quarter:big_final , function(col) { tolower(col) == "fall" }, .names = "fall_{.col}")) |>
+          mutate(across(quarter:big_final , function(col) { tolower(col) == "false start" }, .names = "false_start_{.col}")) |>
+          mutate(across(quarter:big_final, function(col) { ifelse(col == "FALL", NA, col)})) |>
+          mutate(across(quarter:big_final, as.numeric)) |>
+          mutate(sex = "F", first_round = NA, fall_first_round = NA, false_start_first_round = NA, event_id = e_id) |>
+          bind_rows(df_final_w) -> df_final_w
+      }
+    }
+
+
     
   }
 }
 
-df_overall |>
-  left_join(df_qual) |>
-  left_join(df_final) -> times
+df_overall_m |>
+  left_join(df_qual_m) |>
+  left_join(df_final_m) -> m_times
+
+df_overall_w |>
+  left_join(df_qual_w) |>
+  left_join(df_final_w) -> w_times
 
 ## save times object
-save(times, file = "data/times.Rdata")
+save(m_times, file = "data/mTimes.Rdata")
+save(w_times, file = "data/wTimes.Rdata")
+
 
 ## cleanup
 remote_driver$close()
--- scripts/0_get_data/save_climbers.R
+++ scripts/0_get_data/save_climbers.R
@@ -6,8 +6,11 @@ library(googledrive) ## google drive interface
 ## load events and times
 load("data/events.Rdata")
 load("data/times.Rdata")
+load("data/wTimes.Rdata")
 
 ## restructure data for Tomoa Skip data collection
+
+## Mens
 times |>
   filter(rank <= 16) |> ## people in the final
   select(event_id, rank, fname, lname, country) |>
@@ -16,11 +19,27 @@ times |>
   filter(year >= 2018) |>
   mutate(tomoa_skip = NA) -> data_collection
 
+
+## Womens
+w_times |>
+  filter(rank <= 16) |> ## people in the final
+  select(event_id, rank, fname, lname, country) |>
+  left_join(events, by = "event_id") |>
+  select(event_id, event, start_date, end_date, year, rank, fname, lname, country) |>
+  filter(year >= 2018) |>
+  mutate(tomoa_skip = NA) -> data_collection_w
+
 ## save locally
-write_csv(data_collection, "data/data_collection.csv")
+write_csv(data_collection, "data/data_collection_m.csv")
+write_csv(data_collection_w, "data/data_collection_w.csv")
+
 
 ## upload to drive
-data_collection_drv <- drive_upload("data/data_collection.csv", path = "speed_climbing_tomoa_skip_data_collection", type = "spreadsheet")
+data_collection_drv <- drive_upload("data/data_collection.csv", path = "speed_climbing_tomoa_skip_data_collection_men", type = "spreadsheet")
+data_collection_w_drv <- drive_upload("data/data_collection_w.csv", path = "speed_climbing_tomoa_skip_data_collection_women", type = "spreadsheet")
 
 ## make shared
 drive_share(data_collection_drv, role = "writer", type = "user", emailAddress = "Caleb.Chou@colostate.edu")
+drive_share(data_collection_w_drv, role = "writer", type = "user", emailAddress = "calebchou21@gmail.com")
+
+
