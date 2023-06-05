e4f7487a415d5cf503b3410b7e833deec5b3d1d9
calebchou21 <calebchou21@gmail.com>
2023-06-05 17:32:36 -0600

    Updated sum stats w/ discussed fixes

--- scripts/1_calculate_statistics/summary_stats.R
+++ scripts/1_calculate_statistics/summary_stats.R
@@ -1,42 +1,35 @@
 # Required library
 library(dplyr)
 
+load("data/mTimes_skip.Rdata")
+load("data/wTimes_skip.Rdata")
+
 # Group the data frame by year and calculate the percentage of TRUE tomoa_skip values
 # Calculate average final times where tomoa skip is true and where tomoa skip is false.
 m_tomoa_skip_yearly <- m_times_update |>
   group_by(year) |>
   summarise(total_count = n(),
-            false_count = sum(tomoa_skip == FALSE),
-            true_count = sum(tomoa_skip == TRUE),
+            false_count = sum(tomoa_skip, na.rm = TRUE),
+            true_count = sum(!tomoa_skip, na.rm = TRUE),
             percent_true = (true_count / total_count) * 100,
             #These are yearly averages of FINAL times with and without skip
             avg_skip_true = mean(final[tomoa_skip == TRUE], na.rm = TRUE),
             avg_skip_false = mean(final[tomoa_skip == FALSE], na.rm = TRUE),
             #this is OVERALL yearly final average, regardless of tomoa skip 
-            avg_final = mean(final, na.rm = TRUE)
+            avg_final = mean(final, na.rm = TRUE),
             )
 
 w_tomoa_skip_yearly <- w_times_update |>
   group_by(year) |>
   summarise(total_count = n(),
-            false_count = sum(tomoa_skip == FALSE),
-            true_count = sum(tomoa_skip == TRUE),
+            false_count = count(!tomoa_skip),
+            true_count = count(tomoa_skip),
             percent_true = (true_count / total_count) * 100,
             avg_skip_true = mean(final[tomoa_skip == TRUE], na.rm = TRUE),
             avg_skip_false = mean(final[tomoa_skip == FALSE], na.rm = TRUE),
             avg_final = mean(final, na.rm = TRUE),
-            total_falls = sum(fall_qual) + sum(fall_final) +
-              sum(fall_lane_a) + sum(fall_lane_b) + sum(fall_quarter) +
-              sum(fall_semi) + sum(fall_small_final) + sum(fall_big_final) + 
-              sum(fall_first_round)
             )
 
-
-
-
-
-
-            
 
 
 #Find the earliest date at which a tomoa skip is performed for mens and womens
@@ -51,16 +44,4 @@ w_earliest_skip <- min(filtered_data$start_date)
 
 
 
-cat('\n')
-print("Mens Tomoa Skip by Year:")
-print(m_tomoa_skip_yearly)
-cat('\n')
-print("Womens Tomoa Skip by Year:")
-print(w_tomoa_skip_yearly)
-cat('\n')
 
-print("First occurence of tomoa skip for men:")
-print(m_earliest_skip)
-cat('\n')
-print("First occurence of tomoa skip for women:")
-print(w_earliest_skip)
