3166256b33989b759f88462ac46cf3b728c385f9
calebchou21 <calebchou21@gmail.com>
2023-06-11 15:20:55 -0600

    fixed last commit

--- scripts/1_calculate_statistics/summary_stats.R
+++ scripts/1_calculate_statistics/summary_stats.R
@@ -4,133 +4,111 @@ library(dplyr)
 load("data/mTimes_skip.Rdata")
 load("data/wTimes_skip.Rdata")
 
-# -- notes -- 
-# FIXME -- mark as NA
-# avg_final time in 2017 is 28, caused by 999.00 values.
+# Average times for finals
+m_times_update |>
+  group_by(year) |>
+  filter(rank <= 16) |>
+  summarise(
+    num_entries = n(),
+    avg_final_time_ts = mean(final[tomoa_skip], na.rm = TRUE),
+    avg_final_time_no_ts = mean(final[!tomoa_skip], na.rm = TRUE),
+    avg_final_time = mean(final, na.rm = TRUE)
+  )
 
-# Group the data frame by yeasr and calculate the percentage of TRUE tomoa_skip values
-# Calculate average final times where tomoa skip is true and where tomoa skip is false.
-m_tomoa_skip_yearly <- m_times_update |>
+w_times_update |>
+  group_by(year) |>
+  filter(rank <= 16) |>
+  summarise(
+    num_entries = n(),
+    avg_final_time_ts = mean(final[tomoa_skip], na.rm = TRUE),
+    avg_final_time_no_ts = mean(final[!tomoa_skip], na.rm = TRUE),
+    avg_final_time = mean(final, na.rm = TRUE)
+  )
+
+# Average times for qualifiers
+m_times_update |>
   group_by(year) |>
   summarise(
-            total_count = n(),
-            false_count = sum(!tomoa_skip, na.rm = TRUE),
-            true_count = sum(tomoa_skip, na.rm = TRUE),
-            
-            # Yearly avgs of FINAL times for true and false tomoa skip
-            final_avg_false = mean(final[!tomoa_skip], na.rm = TRUE),
-            final_avg_true = mean(final[tomoa_skip], na.rm = TRUE),
-            # Overall yearly final average, regardless of tomoa skip 
-            final_avg_overall = mean(final, na.rm = TRUE),
-            
-            # Total falls in final
-            final_total_falls = sum(fall_final, na.rm = TRUE),
-            # Number of falls in final for climbers NOT the tomoa skip
-            final_falls_false = sum(fall_final & !tomoa_skip, na.rm = TRUE),
-            # Percent of climbers that don't use the skip and fall in the final, out of all climbers who don't use the skip
-            final_falls_percent_false = (sum(fall_final & !tomoa_skip, na.rm = TRUE) / sum(!tomoa_skip, na.rm=TRUE)) * 100,
-            # Number of falls in the final for climbers who DO use the tomoa skip
-            final_falls_true = sum(fall_final & tomoa_skip, na.rm = TRUE),
-            # Percent of climbers that use the skip and fall in the final, out of all climbers who use the skip
-            #final_falls_percent_true = (sum(fall_final & tomoa_skip, na.rm = TRUE) / sum(tomoa_skip, na.rm=TRUE)) * 100,
-            
-            # Total false starts in final
-            final_total_FS = sum(false_start_final, na.rm = TRUE),
-            # Number of false starts in final for climbers not using the skip
-            final_FS_false = sum(false_start_final & !tomoa_skip, na.rm = TRUE),
-            # Percent of climbers who don't use the skip and false start in the final, out of all the climbers who don't use the skip
-            #final_FS_percent_false = (sum(false_start_final & !tomoa_skip, na.rm = TRUE) / sum(!tomoa_skip, na.rm=TRUE)) * 100,
-            # Number of false starts in final for climbers using the skip
-            final_FS_true = sum(false_start_final & tomoa_skip, na.rm = TRUE),
-            # Percent of climbers who use the skip and false start in the final, out of all climbers who use the skip
-            #final_FS_percent_true = (sum(false_start_final & tomoa_skip, na.rm = TRUE) / sum(tomoa_skip, na.rm=TRUE)) * 100,
-            
-            
-            # Yearly avgs of best_qual times for true and false tomoa skip
-            qual_avg_false = mean(best_qual[!tomoa_skip], na.rm = TRUE),
-            qual_avg_true = mean(best_qual[tomoa_skip], na.rm = TRUE),
-            # Overall yearly best_qual average, regardless of tomoa skip
-            qual_avg_overall = mean(best_qual, na.rm = TRUE),
-            
-            # Total falls in qualifiers
-            qual_total_falls = sum(fall_qual, na.rm = TRUE),
-            # Number of qualifying falls for climbers not using the skip
-            qual_falls_false = sum(fall_qual & !tomoa_skip, na.rm = TRUE),
-            # Percent of climbers that don't use the skip and fall in the qualifiers, out of all climbers who don't use the skip
-            #qual_falls_percent_false = (sum(fall_qual & !tomoa_skip, na.rm = TRUE) / sum(!tomoa_skip, na.rm=TRUE)) * 100,
-            # Number of falls in qualifier for climbers using the skip
-            qual_falls_true = sum(fall_qual & tomoa_skip, na.rm = TRUE),
-            # Percent of climbers that use the skip and fall in the qualifiers, out of all climbers who use the skip
-            #qual_falls_percent_true = (sum(fall_qual & tomoa_skip, na.rm = TRUE) / sum(tomoa_skip, na.rm=TRUE)) * 100,
-            
-            #. Total number of false starts in qualifiers
-            qual_total_FS = sum(false_start_qual, na.rm = TRUE),
-            # Number of false starts for climbers not using the tomoa skip
-            qual_FS_false = sum(false_start_qual & !tomoa_skip, na.rm = TRUE),
-            # Percent of climbers who don't use the skip and false start, out of all climbers who don't use the skip
-            #qual_FS_percent_false = (sum(false_start_qual & !tomoa_skip, na.rm = TRUE) / sum(!tomoa_skip, na.rm=TRUE)) * 100,
-            # Number of false starts for climbers using the tomoa skip
-            qual_FS_true = sum(false_start_qual & tomoa_skip, na.rm = TRUE),
-            # Percent of climbers who use the skip and false start in the final, out of all climbers who use the skip
-            #qual_FS_percent_true = (sum(false_start_qual & tomoa_skip, na.rm = TRUE) / sum(tomoa_skip, na.rm=TRUE)) * 100
-            )
+    num_entries = n(),
+    avg_qual_time_ts = mean(best_qual[!tomoa_skip], na.rm = TRUE),
+    avg_qual_time_no_ts = mean(best_qual[tomoa_skip], na.rm = TRUE),
+    avg_qual_time = mean(best_qual, na.rm = TRUE)
+  )
+w_times_update |>
+  group_by(year) |>
+  summarise(
+    num_entries = n(),
+    avg_qual_time_ts = mean(best_qual[!tomoa_skip], na.rm = TRUE),
+    avg_qual_time_no_ts = mean(best_qual[tomoa_skip], na.rm = TRUE),
+    avg_qual_time = mean(best_qual, na.rm = TRUE)
+  )
+
+# Falls and false starts in qualifier
+m_times_update |> 
+  group_by(year) |> 
+  summarise(
+    num_entries = n(), 
+    num_falls_qual_ts = sum(fall_qual & tomoa_skip, na.rm = TRUE), 
+    num_falls_qual_no_ts = sum(fall_qual & !tomoa_skip, na.rm = TRUE),
+    num_FS_qual_ts = sum(false_start_qual & tomoa_skip, na.rm = TRUE),
+    num_FS_qual_no_ts = sum(false_start_qual & !tomoa_skip, na.rm = TRUE),
+    )
+
+w_times_update |> 
+  group_by(year) |> 
+  summarise(
+    num_entries = n(), 
+    num_falls_qual_ts = sum(fall_qual & tomoa_skip, na.rm = TRUE), 
+    num_falls_qual_no_ts = sum(fall_qual & !tomoa_skip, na.rm = TRUE),
+    num_FS_qual_ts = sum(false_start_qual & tomoa_skip, na.rm = TRUE),
+    num_FS_qual_no_ts = sum(false_start_qual & !tomoa_skip, na.rm = TRUE),
+  )
+
+# Falls and false starts in final
+m_times_update |>
+  group_by(year) |>
+  filter(rank <= 16) |>
+  summarise(
+    num_entries = n(),
+    num_falls_final_ts = sum(fall_final & tomoa_skip, na.rm = TRUE),
+    num_falls_final_no_ts = sum(fall_final & !tomoa_skip, na.rm = TRUE),
+    num_TS_final_ts = sum(fall_final & tomoa_skip, na.rm = TRUE),
+    num_TS_final_no_ts = sum(fall_final & !tomoa_skip, na.rm = TRUE)
+)
+
+w_times_update |>
+  group_by(year) |>
+  filter(rank <= 16) |>
+  summarise(
+    num_entries = n(),
+    num_falls_final_ts = sum(fall_final & tomoa_skip, na.rm = TRUE),
+    num_falls_final_no_ts = sum(fall_final & !tomoa_skip, na.rm = TRUE),
+    num_TS_final_ts = sum(fall_final & tomoa_skip, na.rm = TRUE),
+    num_TS_final_no_ts = sum(fall_final & !tomoa_skip, na.rm = TRUE)
+)
+
 
 # Average number of climbers who use tomoa skip in a final
-m_times_update |> filter(rank <= 16) |> group_by(year) |> summarise(num_climbers_final_no_ts = sum(!tomoa_skip), num_climbers_final_ts = sum(tomoa_skip), num_events = length(unique(event_id))) |> mutate(rate_no_ts = num_climbers_final_no_ts / num_events, rate_ts = num_climbers_final_ts / num_events)
-w_times_update |> filter(rank <= 16) |> group_by(year) |> summarise(num_climbers_final_no_ts = sum(!tomoa_skip), num_climbers_final_ts = sum(tomoa_skip), num_events = length(unique(event_id))) |> mutate(rate_no_ts = num_climbers_final_no_ts / num_events, rate_ts = num_climbers_final_ts / num_events)
-
-# Average final times for use of tomoa skip 
-m_times_update |>
-  filter(rank <=16) |>
-  group_by(year) |>
+m_times_update |> 
+  filter(rank <= 16) |> 
+  group_by(year) |> 
   summarise(
-    num_climbers_ts = sum(tomoa_skip),
-    num_climbers_no_ts = sum(!tomoa_skip),
-    final_avg_with_ts = mean(final[tomoa_skip], na.rm = TRUE)
-  )
+    num_climbers_final_no_ts = sum(!tomoa_skip), 
+    num_climbers_final_ts = sum(tomoa_skip), 
+    num_events = length(unique(event_id))
+    ) |> 
+  mutate(rate_no_ts = num_climbers_final_no_ts / num_events, rate_ts = num_climbers_final_ts / num_events)
 
-w_tomoa_skip_yearly <- w_times_update |>
-  group_by(year) |>
+w_times_update |> 
+  filter(rank <= 16) |> 
+  group_by(year) |> 
   summarise(
-    
-    total_count = n(),
-    false_count = sum(!tomoa_skip, na.rm = TRUE),
-    true_count = sum(tomoa_skip, na.rm = TRUE),
-    
-    final_avg_false = mean(final[!tomoa_skip], na.rm = TRUE),
-    final_avg_true = mean(final[tomoa_skip], na.rm = TRUE),
-    final_avg_overall = mean(final, na.rm = TRUE),
-    
-    final_total_falls = sum(fall_final, na.rm = TRUE),
-    final_falls_false = sum(fall_final & !tomoa_skip, na.rm = TRUE),
-    final_falls_percent_false = (sum(fall_final & !tomoa_skip, na.rm = TRUE) / sum(!tomoa_skip, na.rm=TRUE)) * 100,
-    final_falls_true = sum(fall_final & tomoa_skip, na.rm = TRUE),
-    final_falls_percent_true = (sum(fall_final & tomoa_skip, na.rm = TRUE) / sum(tomoa_skip, na.rm=TRUE)) * 100,
-    
-    final_total_FS = sum(false_start_final, na.rm = TRUE),
-    final_FS_false = sum(false_start_final & !tomoa_skip, na.rm = TRUE),
-    final_FS_percent_false = (sum(false_start_final & !tomoa_skip, na.rm = TRUE) / sum(!tomoa_skip, na.rm=TRUE)) * 100,
-    final_FS_true = sum(false_start_final & tomoa_skip, na.rm = TRUE),
-    final_FS_percent_true = (sum(false_start_final & tomoa_skip, na.rm = TRUE) / sum(tomoa_skip, na.rm=TRUE)) * 100,
-    
-  
-    qual_avg_false = mean(best_qual[!tomoa_skip], na.rm = TRUE),
-    qual_avg_true = mean(best_qual[tomoa_skip], na.rm = TRUE),
-    qual_avg_overall = mean(best_qual, na.rm = TRUE),
-    
-    qual_total_falls = sum(fall_qual, na.rm = TRUE),
-    qual_falls_false = sum(fall_qual & !tomoa_skip, na.rm = TRUE),
-    qual_falls_percent_false = (sum(fall_qual & !tomoa_skip, na.rm = TRUE) / sum(!tomoa_skip, na.rm=TRUE)) * 100,
-    qual_falls_true = sum(fall_qual & tomoa_skip, na.rm = TRUE),
-    qual_falls_percent_true = (sum(fall_qual & tomoa_skip, na.rm = TRUE) / sum(tomoa_skip, na.rm=TRUE)) * 100,
-    
-    qual_total_FS = sum(false_start_qual, na.rm = TRUE),
-    qual_FS_false = sum(false_start_qual & !tomoa_skip, na.rm = TRUE),
-    qual_FS_percent_false = (sum(false_start_qual & !tomoa_skip, na.rm = TRUE) / sum(!tomoa_skip, na.rm=TRUE)) * 100,
-    qual_FS_true = sum(false_start_qual & tomoa_skip, na.rm = TRUE),
-    qual_FS_percent_true = (sum(false_start_qual & tomoa_skip, na.rm = TRUE) / sum(tomoa_skip, na.rm=TRUE)) * 100
-    
-  )
+    num_climbers_final_no_ts = sum(!tomoa_skip), 
+    num_climbers_final_ts = sum(tomoa_skip), 
+    num_events = length(unique(event_id))
+  ) |> 
+  mutate(rate_no_ts = num_climbers_final_no_ts / num_events, rate_ts = num_climbers_final_ts / num_events)
+
 
 
 
